fb3019f8ef5b85ac34e63baca6df0e9fc39c588f
gaurav jagpal <gauravjagpal@hotmail.com>
2024-11-20 21:25:07 +0000

    finish comment settings

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include('comments.urls')),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
 ]
