71a3b239da9ec30b149c4ac7a3c2a195d7d422ca
gauravjagpal <46322325+gauravjagpal@users.noreply.github.com>
2024-11-30 23:28:46 +0000

    updated client origin dev

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,10 +67,22 @@ ALLOWED_HOSTS = [
 os.environ.get('ALLOWED_HOST'),
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-            os.environ.get("CLIENT_ORIGIN"),
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    client_origin = os.environ.get('CLIENT_ORIGIN_DEV', '')
+    
+    # Perform regex match
+    match = re.match(r'^.+-', client_origin, re.IGNORECASE)
+    
+    if match:
+        # Extract the matched portion if there's a match
+        extracted_url = match.group(0)
+        CORS_ALLOWED_ORIGIN_REGEXES = [
+            rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
         ]
+    else:
+        # Handle the case when no match is found, e.g., log a warning or set a default
+        print("No match found for CLIENT_ORIGIN_DEV")
+        CORS_ALLOWED_ORIGIN_REGEXES = []
     
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(
