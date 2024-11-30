e819507adec7869fe5602d4045f07c80f14249c6
gauravjagpal <46322325+gauravjagpal@users.noreply.github.com>
2024-11-30 23:29:39 +0000

    update client origin

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,6 +67,11 @@ ALLOWED_HOSTS = [
 os.environ.get('ALLOWED_HOST'),
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+            os.environ.get("CLIENT_ORIGIN"),
+        ]
+    
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     client_origin = os.environ.get('CLIENT_ORIGIN_DEV', '')
     
@@ -83,14 +88,7 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         # Handle the case when no match is found, e.g., log a warning or set a default
         print("No match found for CLIENT_ORIGIN_DEV")
         CORS_ALLOWED_ORIGIN_REGEXES = []
-    
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
